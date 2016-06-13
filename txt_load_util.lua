b371f4594525dfbf4f76d1f71717539b3eb9c1fe
Þorsteinn Hjörtur Jónsson <thj92@hi.is>
2016-06-13 22:31:05 +0000

    buildvocab routine is ready

--- txt_load_util.lua
+++ txt_load_util.lua
@@ -3,54 +3,33 @@
 local dl = require 'dataload'
 local txt_load_util = {}
 -- text utility functions
-
+-- tokens contain all the characters from the whole sequence representing our document.
+-- We are going to build a table containing all the different unique tokens
+-- This will be our vocabulary. The place of each token (which is pseudo random) will generate the character id for the token.
+-- This contains all the information about the building blocks,
+-- The raw data can thus be represented as linear combinations of vectors with dimensions #vocab
 function txt_load_util.buildVocab(tokens)
-    local ischartokens = true -- For now we only work with characters
     assert(torch.type(tokens) == 'table', 'Expecting table')
     assert(torch.type(tokens[1]) == 'string', 'Expecting table of strings')
-    minfreq = -1
-    local charfreq = {}
-
-    -- Build a table containing each token and the respective frequency in data
-    -- Note that we have already tokenized at this stage, so each element
-    -- of the tokens contains an icelandic utf8-coded character
+    local vocab,ivocab = {}
     for i=1,#tokens do
         local char = tokens[i]
-        charfreq[char] = (charfreq[char] or 0) + 1
-    end
-
-    local vocab, ivocab = {}, {}
-    local charseq = 0
-    
-    local _ = require 'moses'
-    -- make sure ordering is consistent
-    local chars = _.sort(_.keys(charfreq))
-    -- Get UTF8 support to determine length of words
-    local oov = 0
-    -- Count order of vocabulary
-    for i, char in ipairs(chars) do
-        local freq = charfreq[char]
-            -- oov = order of vocabulary
-            oov = oov + freq
-    end
-
-        return vocab, ivocab
+		if vocab.char == nil then
+			i = charid
+			table.insert(ivocab,char)
+			vocab.char = charid
+		end 
+	end
+	return vocab, ivocab
 end
 
-function txt_load_util.text2tensor(tokens, vocab)
-    local oov = vocab['<OOV>']--TODO much better implementation exists
+function txt_load_util.text2tensor(ivocab)
     -- Build a tensor with a length which corresponds to size of vocabulary
     -- Each element in the vocab will receive an entry in this one-dimensional tensor
-    local tensor = torch.IntTensor(#tokens):fill(0)
+    local tensor = torch.IntTensor(#vocab):fill(0)
     -- Each number receives word id which corresponds to its frequency
-    for i, char in ipairs(tokens) do
-        local charid = vocab[char] 
-        if not charid then
-            assert(oov)
-            charid = oov
-        end
-
-        tensor[i] = charid
+    for i, char in ipairs(vocab) do
+        tensor[i] = 1
     end
 
     return tensor
