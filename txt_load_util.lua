1fc4d0c7b3199b5d387471051205ca65e4abeade
Þorsteinn Hjörtur Jónsson <thj92@hi.is>
2016-06-14 01:35:35 +0000

    txt_load og txt_load_util routines look good, now we only have to carefully learn to understand the sequence loaders

--- txt_load_util.lua
+++ txt_load_util.lua
@@ -9,27 +9,29 @@ local txt_load_util = {}
 -- This contains all the information about the building blocks,
 -- The raw data can thus be represented as linear combinations of vectors with dimensions #vocab
 function txt_load_util.buildVocab(tokens)
-    assert(torch.type(tokens) == 'table', 'Expecting table')
-    assert(torch.type(tokens[1]) == 'string', 'Expecting table of strings')
-    local vocab,ivocab = {}
+    --assert(torch.type(tokens) == 'table', 'Expecting table')
+    --assert(torch.type(tokens[1]) == 'string', 'Expecting table of strings')
+    local vocab = {}
+	local ivocab = {}
+	local counter = 1
     for i=1,#tokens do
         local char = tokens[i]
-		if vocab.char == nil then
-			i = charid
-			table.insert(ivocab,char)
-			vocab.char = charid
+		if vocab[char] == nil then
+			ivocab[counter] = char
+			vocab[char] = counter
+			counter = counter + 1
 		end 
 	end
 	return vocab, ivocab
 end
 
-function txt_load_util.text2tensor(ivocab)
+function txt_load_util.text2tensor(ivocab,vocab)
     -- Build a tensor with a length which corresponds to size of vocabulary
     -- Each element in the vocab will receive an entry in this one-dimensional tensor
-    local tensor = torch.IntTensor(#vocab):fill(0)
+    local tensor = torch.IntTensor(#ivocab):fill(0)
     -- Each number receives word id which corresponds to its frequency
-    for i, char in ipairs(vocab) do
-        tensor[i] = 1
+    for i, char in ipairs(ivocab) do
+        tensor[i] = vocab[char]
     end
 
     return tensor
