c29b1cf1f58541cc744e75067ec63ac4dbd4aa23
Þorsteinn Hjörtur Jónsson <thj92@hi.is>
2016-06-25 01:51:48 +0000

    added testset loader

--- recurrent-language-model.lua
+++ recurrent-language-model.lua
@@ -23,7 +23,7 @@ cmd:option('--schedule', '', 'learning rate schedule. e.g. {[5] = 0.004, [6] = 0
 cmd:option('--momentum', 0.9, 'momentum')
 cmd:option('--maxnormout', -1, 'max l2-norm of each layer\'s output neuron weights')
 cmd:option('--cutoff', -1, 'max l2-norm of concatenation of all gradParam tensors')
-cmd:option('--cuda', true, 'use CUDA')
+cmd:option('--cuda', false, 'use CUDA')
 cmd:option('--device', 1, 'sets the device (GPU) to use')
 cmd:option('--maxepoch', 1000, 'maximum number of epochs to run')
 cmd:option('--earlystop', 50, 'maximum number of epochs to wait to find a better local minima for early-stopping')
@@ -39,8 +39,8 @@ cmd:option('--hiddensize', '{200}', 'number of hidden units used at output of ea
 cmd:option('--dropout', 0.5, 'apply dropout with this probability after each rnn layer. dropout <= 0 disables it.')
 -- data
 cmd:option('--batchsize', 120, 'number of examples per batch')
-cmd:option('--trainsize', 2*32768, 'number of train examples seen between each epoch')
-cmd:option('--validsize', 2*16384, 'number of valid examples used for early stopping and cross-validation') 
+cmd:option('--trainsize', 2^10, 'number of train examples seen between each epoch')
+cmd:option('--validsize', 2^9, 'number of valid examples used for early stopping and cross-validation') 
 cmd:option('--savepath', './char_results', 'path to directory where experiment log (includes model) will be saved')
 cmd:option('--id', 'althingi_blstm_seqlen100hiddensize1x200trainsize2to16', 'id string of this experiment (used to name output file) (defaults to a unique id)')
 
@@ -64,6 +64,7 @@ train_on_char = true -- if false it trains on words
 bidirectional = true -- if false it is unidirectional
 -- This is where the magic happens:
 -- input: train_on_char (boolean), bidirectional (boolean), bat
+-- Returns the respective loaders 
 local trainset, validset, testset = txt_load.Althingi(train_on_char,bidirectional,{opt.batchsize,1,1})
 print("Char Vocabulary size : "..#trainset.icharvocab) 
 local char_vocabsize = #trainset.icharvocab
@@ -182,6 +183,7 @@ xplog.dataset = 'Althingi'
 --xplog.vocab = trainset.vocab
 xplog.trainset = trainset
 xplog.validset = validset
+xplog.testset = testset
 -- will only serialize params
 xplog.model = nn.Serial(lm)
 xplog.model:mediumSerial()
@@ -194,116 +196,3 @@ xplog.valppl = {}
 xplog.minvalppl = 99999999
 xplog.epoch = 1
 xplog = train.run(xplog)
---local ntrial = 0
---paths.mkdir(opt.savepath)
-
---local epoch = 1
---opt.lr = opt.startlr
---opt.trainsize = opt.trainsize == -1 and trainset:size() or opt.trainsize
---opt.validsize = opt.validsize == -1 and validset:size() or opt.validsize
---while opt.maxepoch <= 0 or epoch <= opt.maxepoch do
---   print("")
---   print("Epoch #"..epoch.." :")
---
---   -- 1. training
---   
---   local a = torch.Timer()
---   lm:training()
---   local sumErr = 0
---   for i, inputs, targets in trainset:subiter(opt.seqlen, opt.trainsize) do
---      targets = targetmodule:forward(targets)
---      
---      -- forward
---      local outputs = lm:forward(inputs)
---      local err = criterion:forward(outputs, targets)
---      sumErr = sumErr + err
---      
---      -- backward 
---      local gradOutputs = criterion:backward(outputs, targets)
---      lm:zeroGradParameters()
---      lm:backward(inputs, gradOutputs)
---      
---      -- update
---      if opt.cutoff > 0 then
---         local norm = lm:gradParamClip(opt.cutoff) -- affects gradParams
---         opt.meanNorm = opt.meanNorm and (opt.meanNorm*0.9 + norm*0.1) or norm
---      end
---      lm:updateGradParameters(opt.momentum) -- affects gradParams
---      lm:updateParameters(opt.lr) -- affects params
---      lm:maxParamNorm(opt.maxnormout) -- affects params
---
---      if opt.progress then
---         xlua.progress(math.min(i + opt.seqlen, opt.trainsize), opt.trainsize)
---      end
---
---      if i % 1000 == 0 then
---         collectgarbage()
---      end
---
---   end
---   
---   -- learning rate decay
---   if opt.schedule then
---      opt.lr = opt.schedule[epoch] or opt.lr
---   else
---      opt.lr = opt.lr + (opt.minlr - opt.startlr)/opt.saturate
---   end
---   opt.lr = math.max(opt.minlr, opt.lr)
---   
---   if not opt.silent then
---      print("learning rate", opt.lr)
---      if opt.meanNorm then
---         print("mean gradParam norm", opt.meanNorm)
---      end
---   end
---
---   if cutorch then cutorch.synchronize() end
---   local speed = a:time().real/opt.trainsize
---   print(string.format("Speed : %f sec/batch ", speed))
---
---   local ppl = torch.exp(sumErr/opt.trainsize)
---   print("Training PPL : "..ppl)
---
---   xplog.trainppl[epoch] = ppl
---
---   -- 2. cross-validation
---
---   lm:evaluate()
---   local sumErr = 0
---   for i, inputs, targets in validset:subiter(opt.seqlen, opt.validsize) do
---      targets = targetmodule:forward(targets)
---      local outputs = lm:forward(inputs)
---      local err = criterion:forward(outputs, targets)
---      sumErr = sumErr + err
---   end
---
---   local ppl = torch.exp(sumErr/opt.validsize)
---   -- Note :
---   -- Perplexity = exp( sum ( NLL ) / #w)
---   -- Bits Per Word = log2(Perplexity)
---   -- Bits per Char = BPW * (#w / #c)
---   print("Validation PPL : "..ppl)
---
---   xplog.valppl[epoch] = ppl
---   ntrial = ntrial + 1
---
---   -- early-stopping
---   if ppl < xplog.minvalppl then
---      -- save best version of model
---      xplog.minvalppl = ppl
---      xplog.epoch = epoch 
---      local filename = paths.concat(opt.savepath, opt.id..'.t7')
---      print("Found new minima. Saving to "..filename)
---      torch.save(filename, xplog)
---      ntrial = 0
---   elseif ntrial >= opt.earlystop then
---      print("No new minima found after "..ntrial.." epochs.")
---      print("Stopping experiment.")
---      break
---   end
---
---   collectgarbage()
---   epoch = epoch + 1
---end
---print("Evaluate model using : ")
---print("th scripts/evaluate-rnnlm.lua --xplogpath "..paths.concat(opt.savepath, opt.id..'.t7')..(opt.cuda and '--cuda' or ''))
