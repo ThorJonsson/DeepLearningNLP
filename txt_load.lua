3a8d51430b47c65a8efa13bba6559eaf603aa08f
Þorsteinn Hjörtur Jónsson <thj92@hi.is>
2016-08-01 14:49:52 +0000

    We have made a new branch where we implement any tests for benchmark comparisons, currently included is the Penn TreeBank dataset (PTB). We hope to implement many different DNNs on this branch in the future

--- txt_load.lua
+++ txt_load.lua
@@ -21,7 +21,7 @@ function txt_load.Althingi(is_bidirectional,batchsize)
     local loaders = {}
     -- There are three different datafiles in dir
     for i,txt_set in ipairs{'train', 'valid', 'test'} do
-        local text = txt_load_util.getAlthingi(txt_set)
+        local text = txt_load_util.get_raw_data(txt_set, '/home/thj92/DeepLearningNLP/Data/')
         -- Build the list of unique characters in the text (charvocab)
         -- icharvocab is the inverted list
         local tokens = txt_load_util.tokenize(text)
@@ -51,6 +51,55 @@ function txt_load.Althingi(is_bidirectional,batchsize)
     end
 
     return unpack(loaders)
+end
+
+-- Loads Penn Tree Bank train, valid, test sets
+function txt_load.PTB(bidirectional,batchsize, datapath, srcurl, vocab, ivocab, wordfreq)
+   -- 1. arguments and defaults
+   if bidirectional == true then
+       print('We have not implemented bidirectional for PTB yet')
+   end
+
+   -- the size of the batch is fixed for SequenceLoaders
+   batchsize = torch.type(batchsize) == 'table' and batchsize or {batchsize, batchsize, batchsize}
+   assert(torch.type(batchsize[1]) == 'number')
+   -- path to directory containing Penn Tree Bank dataset on disk
+   --datapath = datapath or paths.concat(dl.DATA_PATH, 'PennTreeBank')
+   -- URL from which to download dataset if not found on disk.
+   --srcurl = srcurl or 'https://raw.githubusercontent.com/wojzaremba/lstm/master/data/'
+   
+   if vocab then
+      assert(ivocab and wordfreq)
+   end
+
+   -- 2. load raw data, convert to tensor
+   
+   local file = require('pl.file')
+   local stringx = require('pl.stringx')
+   
+   local loaders = {}
+   for i,whichset in ipairs{'train', 'valid', 'test'} do
+      -- download the file if necessary
+      local filename = 'ptb.'..whichset..'.txt'
+      local filepath = paths.concat(datapath, filename)
+      --dl.downloadfile(datapath, srcurl..filename, filepath)
+      local text = file.read(filepath)
+      text = stringx.replace(text, '\n', '<eos>')
+      local tokens = stringx.split(text)
+      if whichset == 'train' and not vocab then
+         vocab, ivocab, wordfreq = dl.buildVocab(tokens)
+      end
+      local tensor = dl.text2tensor(tokens, vocab)
+      
+      -- 3. encapsulate into SequenceLoader
+      local loader = dl.SequenceLoader(tensor, batchsize[i])
+      loader.vocab = vocab
+      loader.ivocab = ivocab
+      loader.wordfreq = wordfreq
+      table.insert(loaders, loader)
+   end
+   
+   return unpack(loaders)
 end
 
 return txt_load
